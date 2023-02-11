fc700a471ce86db7c46c2a1eaeeb88c9ecf6340c
dani <daniele.davino@live.it>
2023-02-11 21:35:07 +0100

    launch ec2 + fastapi

--- src/api/api.py
+++ src/api/api.py
@@ -21,27 +21,58 @@
 # def update_item(item_id: int, item: Item):
 #     return {"item_name": item.name, "item_id": item_id}
 
-from fastapi import FastAPI, Request
+# from fastapi import FastAPI, Request
 
-app = FastAPI()
+# app = FastAPI()
 
 # @app.get("/sum/{a}/{b}")
 # async def read_item(a: int, b: int):
 #     return {"sum": a + b}
 
+
+# def edit(x,y):
+#     x = x*2
+#     y = y*2
+#     return x,y
+
+# @app.post("/sum")
+# async def read_item(request: Request):
+#     data = await request.json()
+#     a = data["a"]
+#     b = data["b"]
+#     a,b = edit(a,b)
+#     return {"sum": a + b}
+
+from fastapi import FastAPI, Query, HTTPException
+from pydantic import BaseModel
+from api.model import predict, convert
+
+app = FastAPI()
+
+# pydantic models
+class StockIn(BaseModel):
+    ticker: str
+    days: int
+
+class StockOut(StockIn):
+    forecast: dict
+    
 @app.get('/')
 def get_root():
-	return {'message': 'Welcome to the spam detection API'}
+    return {'message': 'Welcome to the stock prediction API'}
 
-def edit(x,y):
-    x = x*2
-    y = y*2
-    return x,y
+@app.post("/predict", response_model=StockOut, status_code=200)
+def get_prediction(payload: StockIn):
+    ticker = payload.ticker
+    days = payload.days
 
-@app.post("/sum")
-async def read_item(request: Request):
-    data = await request.json()
-    a = data["a"]
-    b = data["b"]
-    a,b = edit(a,b)
-    return {"sum": a + b}
+    prediction_list = predict(ticker, days)
+
+    if not prediction_list:
+        raise HTTPException(status_code=400, detail="Model not found.")
+
+    response_object = {
+        "ticker": ticker, 
+        "days": days,
+        "forecast": convert(prediction_list)}
+    return response_object
